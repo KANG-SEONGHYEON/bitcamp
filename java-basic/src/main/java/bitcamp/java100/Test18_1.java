1d56036d538541eae4454557082cf68572aef7a7
KANG-SEONGHYEON <wlswlsqud@gmail.com>
2017-10-16 17:15:18 +0900

    '2017.10.16 - Refresh'

---
+++ java-basic/src/main/java/bitcamp/java100/Test18_1.java
@@ -0,0 +1,13 @@
+// ## String 클래스 사용법
+// 개발자가 문자열을 쉽게 다룰 수 있도록 자바에서 기본으로 제공하는 부품.
+
+package bitcamp.java100;
+public class Test18_1 {
+
+	public static void main(String[] args) {
+		String s1;
+		
+		s1 = new String("ABC가각간");
+		
+	}
+}
--- java-project/src/main/java/Score.java
+++ java-project/src/main/java/Score.java
@@ -6,4 +6,31 @@ public class Score {
     int[] subjects = new int[3];
     int sum;
     float aver;
+    
+    static void compute(Score score) {
+        for (int sub : score.subjects) {
+            score.sum += sub;
+        }
+        score.aver = score.sum / 3f;
+    }
+    
+    static void print(Score score) {
+        System.out.printf("%-4s, %4d, %4d, %4d, %4d, %6.1f\n",  
+                score.name, 
+                score.subjects[0], 
+                score.subjects[1], 
+                score.subjects[2], 
+                score.sum, 
+                score.aver);
+    }
+    
+    static void init(Score score, String name, int kor, int eng, int math) {
+        score.name = name;
+        score.subjects[0] = kor;
+        score.subjects[1] = eng;
+        score.subjects[2] = math;
+        
+        //: 성적 데이터를 저장한 김에 바로 계산을 수행하자! 
+        compute(score);
+    }
 }
